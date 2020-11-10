6e8a23a2ba18bcf405209385ce4eb93c1dfaef1c
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-11-10 10:53:28 -0800

    update script for training and testing data accuracy numbers

--- k8_configs/train_test_rrna/train_test.sh
+++ k8_configs/train_test_rrna/train_test.sh
@@ -3,17 +3,24 @@
 # $2 job_count
 # $3 output bucket
 
+MODELS_BUCKET="bailey-k8s/rrna_experiments/train_rrna_testing_10_26_20/"
+OUTPUT_BUCKET="bailey-k8s/rrna_experiments/train_rrna_testing_10_26_20/output/"
+N_THREADS=20
+THRESHOLD="0.01"
+P_THRESHOLD="0.5"
+N_TRAIN_READS=100
+N_TEST_READS=100
+N_NOP58_GAL_READS=$N_TRAIN_READS
+N_IVT_READS=$N_TEST_READS
+N_CBF5_GAL_READS=$N_TEST_READS
+
+#N_CBF5_GLU_READS=100
+#N_NOP58_GLU_READS=100
+
 main() {
-  # $1 model bucket
-  # $2 output bucket
-  # $3 job_count
-  # $4 embed
-  # $5 threshold
-  # $6 p_threshold
-  # $7 n_files
   start=$SECONDS
-  download_files 5
-  run_training_routine "$1" "$2" "$3" "$4" "$5" "$6"
+  download_files
+  run_training_routine
   duration=$(( SECONDS - start ))
   hours=$(( duration / 3600 ))
   minutes=$(( (duration - (hours*3600) ) / 60 ))
@@ -24,16 +31,10 @@ main() {
 download_files() {
   cwd=$(pwd)
   echo "$cwd"
-
-  echo "Download IVT"
-  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/IVT/ IVT
-  tar -xzf IVT/ivt_25S_full_length.tar.gz -C IVT && rm IVT/ivt_25S_full_length.tar.gz
-  tar -xzf IVT/ivt_18S_full_length.tar.gz -C IVT && rm IVT/ivt_18S_full_length.tar.gz
-  sed -i 's='/data/'='$cwd/'=g' "$cwd"/IVT/kube_ivt_full_length_18S.readdb
-  sed -i 's='/data/'='$cwd/'=g' "$cwd"/IVT/kube_ivt_full_length_25S.readdb
-  head -"$1"  "$cwd"/IVT/kube_ivt_full_length_18S.readdb > "$cwd"/IVT/run_ivt.readdb
-  head -"$1"  "$cwd"/IVT/kube_ivt_full_length_25S.readdb > "$cwd"/IVT/run_ivt.readdb
-  echo "ivt Done"
+  echo "Download REFERENCE"
+  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/reference/ reference
+  echo "Download Source"
+  git clone https://github.com/adbailey4/rrna_scripts
 
   echo "Download NOP58_GAL"
   aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/NOP58_GAL/ NOP58_GAL
@@ -41,9 +42,29 @@ download_files() {
   tar -xzf NOP58_GAL/Nop58_GAL_18S_full_length_fast5.tar.gz -C NOP58_GAL && rm NOP58_GAL/Nop58_GAL_18S_full_length_fast5.tar.gz
   sed -i 's='/data/'='$cwd/'=g' "$cwd"/NOP58_GAL/kube_NOP58GAL.2308.full_length.18S.readdb
   sed -i 's='/data/'='$cwd/'=g' "$cwd"/NOP58_GAL/kube_NOP58GAL.2308.full_length.25S.readdb
-  head -"$1"  "$cwd"/NOP58_GAL/kube_NOP58GAL.2308.full_length.18S.readdb > "$cwd"/NOP58_GAL/run_ngal.readdb
-  head -"$1"  "$cwd"/NOP58_GAL/kube_NOP58GAL.2308.full_length.25S.readdb > "$cwd"/NOP58_GAL/run_ngal.readdb
+  head -"$N_NOP58_GAL_READS"  "$cwd"/NOP58_GAL/kube_NOP58GAL.2308.full_length.18S.readdb > "$cwd"/NOP58_GAL/run_ngal.readdb
+  head -"$N_NOP58_GAL_READS"  "$cwd"/NOP58_GAL/kube_NOP58GAL.2308.full_length.25S.readdb >> "$cwd"/NOP58_GAL/run_ngal.readdb
   echo "NOP58_GAL Done"
+
+  echo "Download IVT"
+  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/IVT/ IVT
+  tar -xzf IVT/ivt_25S_full_length.tar.gz -C IVT && rm IVT/ivt_25S_full_length.tar.gz
+  tar -xzf IVT/ivt_18S_full_length.tar.gz -C IVT && rm IVT/ivt_18S_full_length.tar.gz
+  sed -i 's='/data/'='$cwd/'=g' "$cwd"/IVT/kube_ivt_full_length_18S.readdb
+  sed -i 's='/data/'='$cwd/'=g' "$cwd"/IVT/kube_ivt_full_length_25S.readdb
+  head -"$N_IVT_READS"  "$cwd"/IVT/kube_ivt_full_length_18S.readdb > "$cwd"/IVT/run_ivt.readdb
+  head -"$N_IVT_READS"  "$cwd"/IVT/kube_ivt_full_length_25S.readdb >> "$cwd"/IVT/run_ivt.readdb
+  echo "ivt Done"
+
+  echo "Download CBF5_GAL"
+  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/CBF5_GAL/ CBF5_GAL
+  tar -xzf CBF5_GAL/CBF5_GAL_25S_full_length.tar.gz -C CBF5_GAL && rm CBF5_GAL/CBF5_GAL_25S_full_length.tar.gz
+  tar -xzf CBF5_GAL/CBF5_GAL_18S_full_length.tar.gz -C CBF5_GAL && rm CBF5_GAL/CBF5_GAL_18S_full_length.tar.gz
+  sed -i 's='/data/'='$cwd/'=g' "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.18S.readdb
+  sed -i 's='/data/'='$cwd/'=g' "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.25S.readdb
+  head -"$N_CBF5_GAL_READS"  "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.18S.readdb > "$cwd"/CBF5_GAL/run_cgal.readdb
+  head -"$N_CBF5_GAL_READS"  "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.25S.readdb >> "$cwd"/CBF5_GAL/run_cgal.readdb
+  echo "CBF5_GAL Done"
 
 #  echo "Download NOP58_GLU"
 #  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/NOP58_GLU/ NOP58_GLU
@@ -51,19 +72,9 @@ download_files() {
 #  tar -xzf NOP58_GLU/Nop58_GLU_18S_full_length.tar.gz -C NOP58_GLU && rm NOP58_GLU/Nop58_GLU_18S_full_length.tar.gz
 #  sed -i 's='/data/'='$cwd/'=g' "$cwd"/NOP58_GLU/kube_Nop58GLU_full_length.18S.readdb
 #  sed -i 's='/data/'='$cwd/'=g' "$cwd"/NOP58_GLU/kube_Nop58GLU_full_length.25S.readdb
-#  head -"$1"  "$cwd"/NOP58_GLU/kube_Nop58GLU_full_length.18S.readdb > "$cwd"/NOP58_GLU/run_nglu.readdb
-#  head -"$1"  "$cwd"/NOP58_GLU/kube_Nop58GLU_full_length.25S.readdb > "$cwd"/NOP58_GLU/run_nglu.readdb
+#  head -"$N_NOP58_GLU_READS"  "$cwd"/NOP58_GLU/kube_Nop58GLU_full_length.18S.readdb > "$cwd"/NOP58_GLU/run_nglu.readdb
+#  head -"$N_NOP58_GLU_READS"  "$cwd"/NOP58_GLU/kube_Nop58GLU_full_length.25S.readdb >> "$cwd"/NOP58_GLU/run_nglu.readdb
 #  echo "NOP58_GLU Done"
-#
-#  echo "Download CBF5_GAL"
-#  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/CBF5_GAL/ CBF5_GAL
-#  tar -xzf CBF5_GAL/CBF5_GAL_25S_full_length.tar.gz -C CBF5_GAL && rm CBF5_GAL/CBF5_GAL_25S_full_length.tar.gz
-#  tar -xzf CBF5_GAL/CBF5_GAL_18S_full_length.tar.gz -C CBF5_GAL && rm CBF5_GAL/CBF5_GAL_18S_full_length.tar.gz
-#  sed -i 's='/data/'='$cwd/'=g' "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.18S.readdb
-#  sed -i 's='/data/'='$cwd/'=g' "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.25S.readdb
-#  head -"$1"  "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.18S.readdb > "$cwd"/CBF5_GAL/run_cgal.readdb
-#  head -"$1"  "$cwd"/CBF5_GAL/kube_CBF5GAL_full_length.25S.readdb > "$cwd"/CBF5_GAL/run_cgal.readdb
-#  echo "CBF5_GAL Done"
 #
 #  echo "Download CBF5_GLU"
 #  aws s3 sync --no-progress s3://bailey-k8s/rrna_yeast_data/CBF5_GLU/ CBF5_GLU
@@ -71,19 +82,13 @@ download_files() {
 #  tar -xzf CBF5_GLU/CBF5_GLU_18S_full_length.tar.gz -C CBF5_GLU && rm CBF5_GLU/CBF5_GLU_18S_full_length.tar.gz
 #  sed -i 's='/data/'='$cwd/'=g' "$cwd"/CBF5_GLU/kube_CBF5GLU_full_length.18S.readdb
 #  sed -i 's='/data/'='$cwd/'=g' "$cwd"/CBF5_GLU/kube_CBF5GLU_full_length.25S.readdb
-#  head -"$1"  "$cwd"/CBF5_GLU/kube_CBF5GLU_full_length.18S.readdb  > "$cwd"/CBF5_GLU/run_cglu.readdb
-#  head -"$1"  "$cwd"/CBF5_GLU/kube_CBF5GLU_full_length.25S.readdb  > "$cwd"/CBF5_GLU/run_cglu.readdb
+#  head -"$N_CBF5_GLU_READS"  "$cwd"/CBF5_GLU/kube_CBF5GLU_full_length.18S.readdb  > "$cwd"/CBF5_GLU/run_cglu.readdb
+#  head -"$N_CBF5_GLU_READS"  "$cwd"/CBF5_GLU/kube_CBF5GLU_full_length.25S.readdb  >> "$cwd"/CBF5_GLU/run_cglu.readdb
 #  echo "CBF5_GLU Done"
 
 }
 
 run_training_routine() {
-  # $1 model bucket
-  # $2 output bucket
-  # $3 job_count
-  # $4 embed
-  # $5 threshold
-  # $6 p_threshold
 
   mkdir models
   mkdir output
@@ -91,27 +96,34 @@ run_training_routine() {
   do
     if [[ "$model" == *.model ]]
     then
-      aws s3 cp --no-progress s3://"$1""$model" models
-      train_sa "$model" "$3" "$4" "$5" "$6"
+      aws s3 cp --no-progress s3://"$MODELS_BUCKET""$model" models
+      train_sa "$model"
       #               tar and upload variant calls
+      aws s3 cp --no-progress train_test.sh s3://"$OUTPUT_BUCKET"
 
       tar -czf "$model".testing.tar.gz -C output/ testing/
-      aws s3 mv --no-progress "$model".testing.tar.gz s3://"$2"
+      aws s3 mv --no-progress "$model".testing.tar.gz s3://"$OUTPUT_BUCKET"
 
       tar -czf "$model".testing_accuracy.tar.gz -C output/ testing_accuracy/
-      aws s3 mv --no-progress "$model".testing_accuracy.tar.gz s3://"$2"
+      aws s3 mv --no-progress "$model".testing_accuracy.tar.gz s3://"$OUTPUT_BUCKET"
 
       tar -czf "$model".testing_accuracy_csvs.tar.gz -C output/ testing_accuracy_csvs/
-      aws s3 mv --no-progress "$model".testing_accuracy_csvs.tar.gz s3://"$2"
+      aws s3 mv --no-progress "$model".testing_accuracy_csvs.tar.gz s3://"$OUTPUT_BUCKET"
+
+      tar -czf "$model".training_accuracy.tar.gz -C output/ training_accuracy/
+      aws s3 mv --no-progress "$model".training_accuracy.tar.gz s3://"$OUTPUT_BUCKET"
+
+      tar -czf "$model".training_accuracy_csvs.tar.gz -C output/ training_accuracy_csvs/
+      aws s3 mv --no-progress "$model".training_accuracy_csvs.tar.gz s3://"$OUTPUT_BUCKET"
 
       tar -czf "$model".training.tar.gz -C output/ training/
-      aws s3 mv --no-progress "$model".training.tar.gz s3://"$2"
+      aws s3 mv --no-progress "$model".training.tar.gz s3://"$OUTPUT_BUCKET"
 
       tar -czf "$model".training_distributions.tar.gz -C output/ training_distributions/
-      aws s3 mv --no-progress "$model".training_distributions.tar.gz s3://"$2"
+      aws s3 mv --no-progress "$model".training_distributions.tar.gz s3://"$OUTPUT_BUCKET"
 
       tar -czf "$model".training_models.tar.gz -C output/ training_models/
-      aws s3 mv --no-progress "$model".training_models.tar.gz s3://"$2"
+      aws s3 mv --no-progress "$model".training_models.tar.gz s3://"$OUTPUT_BUCKET"
 
       rm -r output/*
     fi
@@ -144,7 +156,7 @@ cat << EOF >> run_embed_plot_wrapper.json
   "positions": "/data/reference/yeast_18S_25S_mods.positions",
   "ambig_model": $AMBIG_MODEL,
   "rna": true,
-  "num_threads": ${2:-1},
+  "num_threads": $N_THREADS,
   "debug": false,
   "mod_only": true,
   "force_event": false,
@@ -161,7 +173,7 @@ cat << EOF >> train_config.json
   "signal_alignment_args": {
     "target_regions": null,
     "track_memory_usage": false,
-    "threshold": ${4:-0.01},
+    "threshold": $THRESHOLD,
     "event_table": null,
     "embed": false,
     "delete_tmp": true,
@@ -179,26 +191,9 @@ cat << EOF >> train_config.json
       "kmers_from_reference": false,
       "motifs": null,
       "name": "native_nop58_gal",
-      "probability_threshold": ${5:-0.7},
+      "probability_threshold": $P_THRESHOLD,
       "number_of_kmer_assignments": 10000,
-      "alignment_file": "/data/NOP58_GAL/06_17_19_R941_NOP58GAL_Manny_noU.2308.full_length.25S.sorted.bam",
-      "recursive": false,
-      "assignments_dir": null
-    },
-    {
-      "positions_file": "/data/reference/ivt_yeast_18S_25S_mods.positions",
-      "fast5_dirs": ["/data/"],
-      "bwa_reference": "/data/reference/yeast_25S_18S.fa",
-      "fofns": [],
-      "readdb": "/data/IVT/run_ivt.readdb",
-      "fw_reference": null,
-      "bw_reference": null,
-      "kmers_from_reference": false,
-      "motifs": null,
-      "name": "canonical_ivt",
-      "probability_threshold": ${5:-0.7},
-      "number_of_kmer_assignments": 10000,
-      "alignment_file": "/data/IVT/ivt_20191014_control.2308.full_length.25S.sorted.bam",
+      "alignment_file": "/data/NOP58_GAL/06_17_19_R941_NOP58GAL_Manny_noU.2308.full_length.sorted.bam",
       "recursive": false,
       "assignments_dir": null
     }
@@ -242,7 +237,7 @@ cat << EOF >> train_config.json
   "template_hdp_model": null,
   "complement_hmm_model": null,
   "template_hmm_model": $MODEL_PATH,
-  "job_count": ${2:-1},
+  "job_count": $N_THREADS,
   "debug": false,
   "two_d": false,
   "output_dir": "/data/output/training",
@@ -265,7 +260,7 @@ cat << EOF >> run_config.json
   "signal_alignment_args": {
     "target_regions": null,
     "track_memory_usage": false,
-    "threshold": 0.01,
+    "threshold": $THRESHOLD,
     "event_table": null,
     "embed": false,
     "delete_tmp": true,
@@ -283,9 +278,9 @@ cat << EOF >> run_config.json
       "kmers_from_reference": false,
       "motifs": null,
       "name": "native_nop58_gal",
-      "probability_threshold": ${5:-0.7},
+      "probability_threshold": $P_THRESHOLD,
       "number_of_kmer_assignments": 10000,
-      "alignment_file": "/data/NOP58_GAL/06_17_19_R941_NOP58GAL_Manny_noU.2308.full_length.25S.sorted.bam",
+      "alignment_file": "/data/NOP58_GAL/06_17_19_R941_NOP58GAL_Manny_noU.2308.full_length.sorted.bam",
       "recursive": false,
       "assignments_dir": null
     },
@@ -300,9 +295,26 @@ cat << EOF >> run_config.json
       "kmers_from_reference": false,
       "motifs": null,
       "name": "canonical_ivt",
-      "probability_threshold": ${5:-0.7},
+      "probability_threshold": $P_THRESHOLD,
       "number_of_kmer_assignments": 10000,
-      "alignment_file": "/data/IVT/ivt_20191014_control.2308.full_length.25S.sorted.bam",
+      "alignment_file": "/data/IVT/ivt_20191014_control.2308.full_length.sorted.bam",
+      "recursive": false,
+      "assignments_dir": null
+    },
+        {
+      "positions_file": "/data/reference/yeast_18S_25S_mods.positions",
+      "fast5_dirs": ["/data/"],
+      "bwa_reference": "/data/reference/yeast_25S_18S.fa",
+      "fofns": [],
+      "readdb": "/data/CBF5_GAL/run_cgal.readdb",
+      "fw_reference": null,
+      "bw_reference": null,
+      "kmers_from_reference": false,
+      "motifs": null,
+      "name": "native_cbf5_gal",
+      "probability_threshold": $P_THRESHOLD,
+      "number_of_kmer_assignments": 10000,
+      "alignment_file": "/data/CBF5_GAL/06102019_R941_CBF5GAL_noU.2308.full_length.sorted.bam",
       "recursive": false,
       "assignments_dir": null
     }
@@ -346,7 +358,7 @@ cat << EOF >> run_config.json
   "template_hdp_model": null,
   "complement_hmm_model": null,
   "template_hmm_model": $MODEL_PATH,
-  "job_count": ${2:-1},
+  "job_count": $N_THREADS,
   "debug": false,
   "two_d": false,
   "output_dir": "/data/output/training",
@@ -374,10 +386,17 @@ cat << EOF >> train_test_config.json
     "rna": true
   },
   "plot_accuracies": {
-    "positions_files":
-    ["/data/reference/native_yeast_18S_25S_mods_guess.positions",
-      "/data/reference/ivt_yeast_18S_25S_mods.positions"],
-    "names": ["native_nop58_gal", "canonical_ivt"]
+    "train": {
+      "positions_files":
+      ["/data/reference/native_yeast_18S_25S_mods_guess.positions"],
+      "names": ["native_nop58_gal"]
+    },
+    "test": {
+      "positions_files":
+      ["/data/reference/native_yeast_18S_25S_mods_guess.positions",
+        "/data/reference/ivt_yeast_18S_25S_mods.positions"],
+      "names": ["native_cbf5_gal", "canonical_ivt"]
+    }
   },
   "plot_distributions": "/data/run_embed_plot_wrapper.json",
   "options": {
@@ -389,7 +408,7 @@ cat << EOF >> train_test_config.json
 }
 EOF
 #               run signalAlign and variant caller
-/data/rrna_scripts/train_test_accuracy_wrapper.py --config train_test_config.json
+/data/rrna_scripts/scripts/train_test_accuracy_wrapper.py --config train_test_config.json
 }
 
 main "$1" "$2" "$3" "$4" "$5"
